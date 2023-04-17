276657476d8b24cdb9fad7130eae059f761192bf
Alex Bzdel <ab913@duke.edu>
2023-04-17 20:42:41 +0000

    finished implementing create step

--- cluster-rs/src/main.rs
+++ cluster-rs/src/main.rs
@@ -1,10 +1,23 @@
 use clap::{App, Arg, SubCommand};
+use std::fs::File;
+use std::io::prelude::*;
+use std::process::Command;
+use serde::{Serialize};
 
-fn get_size(size: &str) -> &str {
+#[derive(Serialize)]
+struct Cluster {
+    cluster_name: String,
+    spark_version: String,
+    node_type: String,
+    spark_conf: serde_json::Value,
+    num_workers: u32,
+}
+
+fn get_node_type(purpose: &str) -> &str {
     /*Takes desired purpose of cluster (string) and returns type of cluster we will use string.
     String returned is the name of the compute resource we want to use.
     Hardcoded into four sections for now.*/
-    match size {
+    match purpose {
         "general" => "Standard_D96ds_v5",
         "memory" => "Standard_E96ds_v5",
         "storage" => "Standard_L80as_v3",
@@ -13,7 +26,41 @@ fn get_size(size: &str) -> &str {
     }
 }
 
+// make sure create_cluster returns Restul or Option to accept ?
+fn create_cluster(name: &str, purpose: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let node_type = get_node_type(purpose);
+    println!("Creating cluster '{}' with size {}", name, node_type);
+
+    // create json file with cluster info
+    let cluster = Cluster {
+        cluster_name: String::from(name),
+        spark_version: String::from("7.3.x-scala2.12"),
+        node_type: String::from(node_type),
+        spark_conf: serde_json::json!({
+            "spark.speculation": true
+        }),
+        num_workers: 25,
+    };
+
+    let json = serde_json::to_string_pretty(&cluster)?;
+
+    let mut file = File::create("cluster.json")?;
+    file.write_all(json.as_bytes())?;
+
+    // run command line to create cluster
+    let _output = Command::new("databricks")
+        .arg("clusters")
+        .arg("create")
+        .arg("--json-file")
+        .arg("cluster.json")
+        .output()
+        .expect("failed to execute process");
+
+    Ok(())
+}
+
 fn main() {
+
     let matches = App::new("Cluster Management Tool")
         .version("1.0")
         .author("Alex Bzdel")
@@ -24,16 +71,16 @@ fn main() {
                 .arg(
                     Arg::with_name("name")
                         .help("Name of the new cluster")
-                        .short('n')
-                        .required(true),
+                        .required(true)
+                        .index(1),
 
                 )
                 .arg(
                     Arg::with_name("optimize")
                         .help("Optimize for a specific purpose")
-                        .short('o')
                         .required(false)
-                        .default_value("general"),
+                        .default_value("general")
+                        .index(2),
 
                 ),
         )
@@ -44,10 +91,8 @@ fn main() {
     match matches.subcommand() {
         Some(("create", sub_matches)) => {
             let name = sub_matches.value_of("name").unwrap();
-            let size = sub_matches
-                .value_of("size")
-                .unwrap_or_else(|| matches.value_of("size").unwrap());
-            println!("Creating cluster '{}' with size {}", name, size);
+            let _output = create_cluster(name, sub_matches.value_of("optimize").unwrap());
+            println!("Cluster created!");
         }
         Some(("edit", _)) => println!("Editing cluster..."),
         Some(("delete", _)) => println!("Deleting cluster..."),
