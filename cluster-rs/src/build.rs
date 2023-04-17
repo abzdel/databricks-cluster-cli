a1415d7dd921f4b3ac2d76207d350b308e8179b8
Alex Bzdel <ab913@duke.edu>
2023-04-17 20:05:49 +0000

    added create, edit, delete boilerplate. implementing now

--- cluster-rs/src/build.rs
+++
@@ -1,8 +0,0 @@
-use std::env;  
-use copy_to_output::copy_to_output;  
-  
-fn main() {  
-    // Re-runs script if any files in res are changed  
-    println!("cargo:rerun-if-changed=res/*");  
-    copy_to_output("res", &env::var("PROFILE").unwrap()).expect("Could not copy");  
-}
--- cluster-rs/src/main.rs
+++ cluster-rs/src/main.rs
@@ -1,28 +1,39 @@
-// use clap with app and arg
-extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let matches = App::new("Databricks Cluster Manager")
+    let matches = App::new("Cluster Management Tool")
         .version("1.0")
-        .author("Alex Bzdel")
-        .about("A command-line tool for managing Databricks clusters")
-        .arg(Arg::with_name("name")
-             .short('n')
-             .long("name")
-             .value_name("NAME")
-             .help("Sets the name of the cluster")
-             .required(true))
-        .arg(Arg::with_name("size")
-             .short('s')
-             .long("size")
-             .value_name("SIZE")
-             .help("Sets the size of the cluster")
-             .required(true))
+        .author("Your Name <your.name@example.com>")
+        .about("Manage clusters")
+        .subcommand(
+            SubCommand::with_name("create")
+                .about("Create a new cluster")
+                .arg(
+                    Arg::with_name("name")
+                        .help("Name of the new cluster")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("size")
+                        .help("Size of the new cluster")
+                        .required(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("edit").about("Edit an existing cluster"))
+        .subcommand(SubCommand::with_name("delete").about("Delete an existing cluster"))
         .get_matches();
 
-    let name = matches.value_of("name").unwrap();
-    let size = matches.value_of("size").unwrap();
-
-    println!("Creating cluster {} with size {}...", name, size);
+    match matches.subcommand() {
+        Some(("create", sub_matches)) => {
+            let name = sub_matches.value_of("name").unwrap();
+            let size = sub_matches
+                .value_of("size")
+                .unwrap_or_else(|| matches.value_of("size").unwrap());
+            println!("Creating cluster '{}' with size {}", name, size);
+        }
+        Some(("edit", _)) => println!("Editing cluster..."),
+        Some(("delete", _)) => println!("Deleting cluster..."),
+        None => println!("No subcommand specified."),
+        _ => unreachable!(),
+    }
 }
